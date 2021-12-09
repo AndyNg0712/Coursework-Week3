cf65fe4e39a40db0ca7327f5a035c54a8d4e8422
AndyNg0712 <annguyen_2002@yahoo.com>
2021-12-09 23:39:09 +1100

    javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@ var pass=document.getElementById("passwords");
 
  function genPassword() {
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz !#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = 12;
+    var passwordLength = 11;
     var pass = "";
  for (var i = 0; i <= passwordLength; i++) {
    var randomNumber = Math.floor(Math.random() * chars.length);
    pass += chars.substring(randomNumber, randomNumber +1, randomNumber +2);
   }
         document.getElementById("passwords").value = pass;
- };
+ };s
